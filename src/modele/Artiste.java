cddfaa8a417cac3db183ec4225c14e7a6c74a747
eliottwtzl <eliott.wetzel@edu.ece.fr>
2026-04-06 16:34:26 +0200

    artiste Morceau

--- src/modele/Artiste.java
+++ src/modele/Artiste.java
@@ -1,4 +1,35 @@
 package modele;
 
+import java.util.ArrayList;
+
 public class Artiste {
+    private String nom;
+    private ArrayList<Morceau> morceaux;
+    private ArrayList<Album> albums;
+
+    public Artiste(String nom) {
+        this.nom = nom;
+        this.morceaux = new ArrayList<>();
+        this.albums = new ArrayList<>();
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void ajouterMorceau(Morceau morceau) {
+        morceaux.add(morceau);
+    }
+
+    public void ajouterAlbum(Album album) {
+        albums.add(album);
+    }
+
+    public ArrayList<Morceau> getMorceaux() {
+        return morceaux;
+    }
+
+    public ArrayList<Album> getAlbums() {
+        return albums;
+    }
 }
--- src/modele/Morceau.java
+++ src/modele/Morceau.java
@@ -4,32 +4,51 @@ import java.util.ArrayList;
 
 public class Morceau {
     private String titre;
-    private float duree;
+    private float duree; // en secondes
     private String artiste;
     private ArrayList<Album> albums;
+    private int nombreEcoutes;
 
     public Morceau(String titre, float duree, String artiste) {
         this.titre = titre;
         this.duree = duree;
         this.artiste = artiste;
         this.albums = new ArrayList<>();
+        this.nombreEcoutes = 0;
     }
 
     public String getTitre() {
         return titre;
     }
+
     public float getDuree() {
         return duree;
     }
+
     public String getArtiste() {
         return artiste;
     }
+
     public ArrayList<Album> getAlbums() {
         return albums;
     }
 
-    //  ajouter un album
+    public int getNombreEcoutes() {
+        return nombreEcoutes;
+    }
+
     public void ajouterAlbum(Album album) {
-        albums.add(album);
+        if (!albums.contains(album)) {
+            albums.add(album);
+        }
+    }
+
+    public void ecouter() {
+        nombreEcoutes++;
+    }
+
+    @Override
+    public String toString() {
+        return titre + " - " + artiste;
     }
 }
